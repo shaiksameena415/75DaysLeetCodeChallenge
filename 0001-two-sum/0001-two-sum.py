bf56d95cb353dfdee1d81b3d85752bc32973fa51
Shaik Sameena <139790285+shaiksameena415@users.noreply.github.com>
2026-03-12 22:54:51 +0530

    Update 0001-two-sum.py
    Removed method signature and docstring from twoSum.

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,5 @@
 class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+    
         seen = {}
         
         for i, num in enumerate(nums):
